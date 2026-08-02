1f33e2845819b2076b2a2e0ce12a87d7efcde97b
Pat Brown <pbrown@nvidia.com>
2026-08-02 16:26:20 -0400

    Update TSL Tampermonkey support for the new server setup.

--- techsideline.user.js
+++ techsideline.user.js
@@ -1,19 +1,30 @@
 // ==UserScript==
 // @name         TechSideline
-// @version      3
+// @version      4
 // @description  Miscellaneous enchancements for Tech Sideline.
-// @match        https://virginiatech.sportswar.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=sportswar.com
+// @match        https://techsideline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=techsideline.com
 // @grant        GM_addStyle
 // ==/UserScript==
 
 // Add various CSS styles.
 var css =
-    ":has(nav.navbar) { width: 100% !important } " +
-    ".customer-img { width: 30px !important } " +
-    ".customer-img { height: 30px !important } " +
-    "div[class*='SiteTagLine'] { display: none !important } " +
-    "div[class*='MiniHeaderWrap'] { padding: 0px 0px !important } " +
-    "div.sw_freestar_container { display: none !important } " +
+    // Hide ad placeholders.
+    "div:has(.container [data-ad-slot-key='above_header']) { display: none !important } " +
+    "div:has(>a[rel*='sponsored']) { display: none !important } " +
+    "div:has(>div.freestar-ad-slot) { display: none !important } " +
+
+    // Make the sticky posts take less vertical space.
+    "div:has(>span[title='Sticky Post']) { padding-top: 3px !important; padding-bottom: 3px !important } " +
+
+    // Remove excess whitespace at the top of the main board area.
+    "main { padding-top: 0px !important } " +
+
+    // Remove extra space between threads in the classic view.
+    "div.thread-starter { padding-top: 3px !important } " +
+
+    // Shrink the logo to avoid extra space on the page.
+    "img[src*='tsl_logo_circular_337px_wide.png'] { transform: scale(0.7) } " +
+
     "";
 GM_addStyle(css);
